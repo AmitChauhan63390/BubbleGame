60905cb7bf300788cf5cd66334a6c17981923a3a
amit <amit63390@gmail.com>
2023-10-16 20:34:53 +0530

    loop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var clutter="";
-for(var i = 0;i<=107;i++){
-    clutter += `<div class="bubble">5</div>`
+for(var i = 0;i<=113;i++){
+    var rn = Math.round(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>`
     
 }
 
